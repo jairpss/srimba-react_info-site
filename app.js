e747b4eca3ac53b4f1c2e213a861991f9678235e
Jair Partida <jairpartida1@gmail.com>
2021-11-19 00:02:18 -0700

    starting with jsx

---
+++ app.js
@@ -0,0 +1,14 @@
+const page = (
+    <div>
+        <h1>My awesome website in React</h1>
+        <h3>Reasons I love React</h3>
+        <ol>
+            <li>It's composable</li>
+            <li>It's declarative</li>
+            <li>It's a hireable skill</li>
+            <li>It's actively maintained by skilled people</li>
+        </ol>
+    </div>
+)
+
+document.getElementById("root").append(JSON.stringify(page))
