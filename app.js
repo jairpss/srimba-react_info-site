cb79850c0db5616689dd3c12d3f1b3f538f680f4
Jair Partida <jairpartida1@gmail.com>
2021-11-19 00:23:49 -0700

    starting with custom components

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const page = (
     <div>
-        <h1>My awesome website in React</h1>
-        <h3>Reasons I love React</h3>
-        <ol>
-            <li>It's composable</li>
-            <li>It's declarative</li>
-            <li>It's a hireable skill</li>
-            <li>It's actively maintained by skilled people</li>
-        </ol>
+        {/* <img src="./react-logo.png" width="40px" /> */}
+        <h1>Fun facts about React</h1>
+        <ul>
+            <li>Was first released in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 100K stars on GitHub</li>
+            <li>Is maintained by Facebook</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
     </div>
 )
 
-document.getElementById("root").append(JSON.stringify(page))
+ReactDOM.render(page, document.getElementById("root"))
+
