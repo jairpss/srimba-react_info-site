db56d5e53920b16d7ffa11b5de1879e7a9d2876b
Jair Partida <jairpartida1@gmail.com>
2021-11-21 12:30:49 -0700

    create a react app

---
+++ info-site/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from "react"
+import icon from "../reactjs-icon.png"
+
+function Navbar(){
+    return (
+    <div>
+        <img src={icon} alt="logo"/>
+        <h1>Fun facts about React</h1>
+    </div>
+    )
+}
+
+export default Navbar
