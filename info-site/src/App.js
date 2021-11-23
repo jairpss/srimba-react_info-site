9da02bc68312eb7bc3537f75d336feb8c44ef91d
Jair Partida <jairpartida1@gmail.com>
2021-11-22 21:19:20 -0700

    info site done

--- info-site/src/App.js
+++ info-site/src/App.js
@@ -5,7 +5,7 @@ import Main from "./components/Main"
 
   function App() {
     return (
-        <div>
+        <div className="container">
             <Navbar />
             <Main />
         </div>
--- info-site/src/components/Navbar.js
+++ info-site/src/components/Navbar.js
@@ -3,10 +3,11 @@ import icon from "../reactjs-icon.png"
 
 function Navbar(){
     return (
-    <div>
-        <img src={icon} alt="logo"/>
-        <h1>Fun facts about React</h1>
-    </div>
+    <nav>
+        <img src={icon} alt="logo" className="nav--logo"/>
+        <h3 className="nav--logo_text">ReactFacts</h3>
+        <h4 className="nav--title">React Course - Project 1</h4>
+    </nav>
     )
 }
 
